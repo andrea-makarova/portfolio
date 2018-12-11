b57597da485a875fb1fc4f68c9029063c9f16ffc
Andrea Makarová <ajka@desktop-r5oiqdu.kea.local>
2018-12-11 12:50:09 +0100

    first pages done

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+document.getElementById("btn2").onclick = function () {
+    location.href = "skills.html";
+};
+
+document.getElementById("btn1").onclick = function () {
+    location.href = "field.html";
+};
+
